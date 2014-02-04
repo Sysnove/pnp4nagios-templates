4918be0a037164b7a2e66b9c919062f57f76447a
Guillaume Subiron <maethor@subiron.org>
2014-02-04 10:08:36 +0100

    check_swap_paging_rate.php

--- check_swap_paging_rate.php
+++ check_swap_paging_rate.php
@@ -5,23 +5,23 @@
 # Plugin: check_ldap3
 #
 
-$ds_name[0] = "Swap paging rate";
-$opt[0] = "--vertical-label \"$UNIT[0]\" --title \"$hostname / $ds_name[0]\" ";
-$def[0] = rrd::def("in", $RRDFILE[0], $DS[0], "AVERAGE" );
-$def[0] .= rrd::cdef("ininv", "in,-1,*" );
-$def[0] .= rrd::def("out", $RRDFILE[1], $DS[1], "AVERAGE" );
-$def[0] .= rrd::line1("ininv", "#FF0000", "IN");
-$def[0] .= rrd::gprint("in", array("LAST", "AVERAGE", "MAX"), "%7.2lf $UNIT[0]");
-$def[0] .= rrd::line1("out", "#00FF00", "OUT");
-$def[0] .= rrd::gprint("out", array("LAST", "AVERAGE", "MAX"), "%7.2lf $UNIT[1]");
+$ds_name[1] = "Swap paging rate";
+$opt[1] = "--vertical-label \"$UNIT[1]\" --title \"$hostname / $ds_name[1]\" ";
+$def[1] = rrd::def("in", $RRDFILE[1], $DS[1], "AVERAGE" );
+$def[1] .= rrd::cdef("ininv", "in,-1,*" );
+$def[1] .= rrd::def("out", $RRDFILE[2], $DS[2], "AVERAGE" );
+$def[1] .= rrd::line1("ininv", "#FF0000", "IN");
+$def[1] .= rrd::gprint("in", array("LAST", "AVERAGE", "MAX"), "%7.2lf $UNIT[1]");
+$def[1] .= rrd::line1("out", "#00FF00", "OUT");
+$def[1] .= rrd::gprint("out", array("LAST", "AVERAGE", "MAX"), "%7.2lf $UNIT[2]");
 
-if ($WARN[0] != "") {
-    $def[0] .= rrd::hrule( $WARN[0], "#ffff00", "Warning on $WARN[0] $UNIT[0]\\n" );
-    $def[0] .= rrd::hrule( $WARN[0]*-1, "#ffff00");
+if ($WARN[1] != "") {
+    $def[1] .= rrd::hrule( $WARN[1], "#ffff00", "Warning on $WARN[1] $UNIT[1]\\n" );
+    $def[1] .= rrd::hrule( $WARN[1]*-1, "#ffff00");
 }
-if ($CRIT[0] != "") {
-    $def[0] .= rrd::hrule( $CRIT[0], "#ff0000", "Critical on $CRIT[0] $UNIT[0]\\n" );
-    $def[0] .= rrd::hrule( $CRIT[0]*-1, "#ffff00");
+if ($CRIT[1] != "") {
+    $def[1] .= rrd::hrule( $CRIT[1], "#ff0000", "Critical on $CRIT[1] $UNIT[1]\\n" );
+    $def[1] .= rrd::hrule( $CRIT[1]*-1, "#ffff00");
 }
 $def[$j] .= rrd::hrule(0, "#606060");
 
