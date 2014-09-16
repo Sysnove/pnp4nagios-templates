4df6ac5dcf21f51a40dc53bd6239359d80730d02
Guillaume Subiron <maethor@subiron.org>
2014-09-16 15:39:45 +0200

    Line2 on diskstat

--- check_all_diskstat.php
+++ check_all_diskstat.php
@@ -18,7 +18,7 @@ for ($i=1; $i<=count($DS); $i+=3) {
     $u = $UNIT[$i];
     $opt[$j] = "--vertical-label \"$u\" --title \"$hostname / Transactions $ds_name[$j]\" $options $colors ";
     $def[$j] = rrd::def("tps", $RRDFILE[$i], $DS[$i], "AVERAGE");
-    $def[$j] .= rrd::line1("tps", $main_color, "tps");
+    $def[$j] .= rrd::line2("tps", $main_color, "tps");
     $def[$j] .= rrd::gprint("tps", array("LAST", "AVERAGE", "MAX"), "%7.2lf $u");
 
     $j++;
@@ -27,11 +27,11 @@ for ($i=1; $i<=count($DS); $i+=3) {
     $ds_name[$j] = explode("_", $LABEL[$i+1])[1];
     $opt[$j] = "--vertical-label \"$u\" --title \"$hostname / I/O $ds_name[$j]\" $options $colors";
     $def[$j] = rrd::def("read", $RRDFILE[$i+1], $DS[$i+1], "AVERAGE");
-    $def[$j] .= rrd::line1("read", $main_color, "read");
+    $def[$j] .= rrd::line2("read", $main_color, "read");
     $def[$j] .= rrd::gprint("read", array("LAST", "AVERAGE", "MAX"), "%7.2lf $u");
     $def[$j] .= rrd::def("write", $RRDFILE[$i+2], $DS[$i+2], "AVERAGE");
     $def[$j] .= rrd::cdef("writeinv", "write,-1,*");
-    $def[$j] .= rrd::line1("writeinv", $secondary_color, "write");
+    $def[$j] .= rrd::line2("writeinv", $secondary_color, "write");
     $def[$j] .= rrd::gprint("write", array("LAST", "AVERAGE", "MAX"), "%7.2lf $u");
     $def[$j] .= rrd::hrule(0, $black);
 
