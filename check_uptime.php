ff5f7198ccdc232c93c0af9c9af3cd87e8788c22
Guillaume Subiron <maethor@subiron.org>
2014-09-21 20:18:59 +0200

    Error check in check_uptime

--- check_uptime.php
+++ check_uptime.php
@@ -6,14 +6,16 @@
 
 include("style.php");
 
-$ds_name[1] = "$NAGIOS_AUTH_SERVICEDESC";
-$opt[1] = "--vertical-label \"Days\" --title \"Uptime for $hostname\" $options $colors ";
+if (count($DS) >=2) {
+    $ds_name[1] = "$NAGIOS_AUTH_SERVICEDESC";
+    $opt[1] = "--vertical-label \"Days\" --title \"Uptime for $hostname\" $options $colors ";
 
-$def[1] = rrd::def("var1", $RRDFILE[2], $DS[2], "AVERAGE");
-$def[1] .= rrd::cdef("var1_d", "var1,1440,/");
+    $def[1] = rrd::def("var1", $RRDFILE[2], $DS[2], "AVERAGE");
+    $def[1] .= rrd::cdef("var1_d", "var1,1440,/");
 
-//$def[1] .= rrd::area("var1_d", "#A2A2A2FF");
-$def[1] .= rrd::line2("var1_d", $main_color, "Uptime");
-$def[1] .= rrd::gprint("var1_d", "LAST", "%7.0lf days");
+    //$def[1] .= rrd::area("var1_d", "#A2A2A2FF");
+    $def[1] .= rrd::line2("var1_d", $main_color, "Uptime");
+    $def[1] .= rrd::gprint("var1_d", "LAST", "%7.0lf days");
+}
 
 ?>
