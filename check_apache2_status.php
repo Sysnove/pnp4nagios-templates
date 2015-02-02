389188da15cb5283703d3957dae66f618c9c6888
Guillaume Subiron <maethor@subiron.org>
2015-02-02 17:31:12 +0100

    Debug check_apache2_status

--- check_apache2_status.php
+++ check_apache2_status.php
@@ -8,42 +8,38 @@ $color = array(
         'wait' => '#e5ca44',
         );
 
-$opt[1] = "--vertical-label \"Apache2 Uptime\" --title \"Server $hostname\" $options $colors ";
+$opt[1] = "--vertical-label \"Days\" --title \"$hostname / Apache2 Uptime\" $options $colors ";
 $ds_name[1] = "Apache2 Uptime";
 
 $def[1] = rrd::def("var1", $RRDFILE[1], $DS[1]);
+$def[1] .= rrd::cdef("var1_d", "var1,1440,/");
+$def[1] .= rrd::line2("var1_d", $main_color, "Uptime");
+$def[1] .= rrd::gprint("var1_d", array("LAST", "AVERAGE", "MAX"), "%7.0lf days");
 
-$opt[1] = "--vertical-label \"Apache2 Requests\" --title \"Server $hostname\" $options $colors ";
-$ds_name[1] = "Apache2 Requests";
+$opt[2] = "--vertical-label \"Req/Sec\" --title \"$hostname / Apache2 Requests\" $options $colors ";
+$ds_name[2] = "Apache2 Requests";
 
-$def[1] = rrd::def("var5", $RRDFILE[5], $DS[5]);
-$def[1] .= rrd::def("var6", $RRDFILE[6], $DS[6]);
-$def[1] .= rrd::line2("var5", $main_color, "ReqPerSec");
-$def[1] .= rrd::gprint("var5", array("LAST", "AVERAGE", "MAX"), "%.3lf");
-$def[1] .= rrd::line2("var6", $secondary_color, "ConnPerSec");
-$def[1] .= rrd::gprint("var6", array("LAST", "AVERAGE", "MAX"), "%.3lf");
+$def[2] = rrd::def("var2", $RRDFILE[2], $DS[2]);
+$def[2] .= rrd::line2("var2", $main_color, "ReqPerSec");
+$def[2] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%.3lf");
 
-$opt[2] = "--vertical-label \"Req/Conn\" --title \"Server $hostname\" $options $colors ";
-$ds_name[2] = "Nginx Requests";
+$opt[3] = "--vertical-label \"Bytes/Sec\" --title \"$hostname / Apache2 Traffic\" $options $colors ";
+$ds_name[3] = "Apache2 Traffic";
 
-$def[2] = rrd::def("var7", $RRDFILE[7], $DS[7]);
-$def[2] .= rrd::line2("var7", $main_color, "ReqPerConn ");
-$def[2] .= rrd::gprint("var7", array("LAST", "AVERAGE", "MAX"), "%.3lf");
+$def[3] = rrd::def("var3", $RRDFILE[3], $DS[3]);
+$def[3] .= rrd::line2("var3", $main_color, "BytesPerSec");
+$def[3] .= rrd::gprint("var3", array("LAST", "AVERAGE", "MAX"), "%.3lf");
 
-$opt[3] = "--vertical-label \"Threads\"  --title \"Apache2 Workers\" $options $colors ";
-$ds_name[3] = "Apache2 Workers";
 
-$def[3] = rrd::def("var1", $RRDFILE[1], $DS[1]);
-$def[3] .= rrd::def("var2", $RRDFILE[2], $DS[2]);
-$def[3] .= rrd::def("var3", $RRDFILE[3], $DS[3]);
-$def[3] .= rrd::def("var4", $RRDFILE[4], $DS[4]);
-$def[3] .= rrd::area("var3", $color["wait"], "Waiting");
-$def[3] .= rrd::gprint("var3", array("LAST", "AVERAGE", "MAX"), "%.0lf");
-$def[3] .= rrd::area("var1", $color["read"], "Reading", $stack=True);
-$def[3] .= rrd::gprint("var1", array("LAST", "AVERAGE", "MAX"), "%.0lf");
-$def[3] .= rrd::area("var2", $color["write"], "Writing", $stack=True);
-$def[3] .= rrd::gprint("var2", array("LAST", "AVERAGE", "MAX"), "%.0lf");
-$def[3] .= rrd::line2("var4", $black, "Total Active");
-$def[3] .= rrd::gprint("var4", array("LAST", "AVERAGE", "MAX"), "%.0lf");
+$opt[4] = "--vertical-label \"Workers\" --title \"$hostname / Apache2 Workers\" $options $colors ";
+$ds_name[4] = "Apache2 Workers";
+
+$def[4] = rrd::def("active", $RRDFILE[4], $DS[4]);
+$def[4] .= rrd::def("idle", $RRDFILE[5], $DS[5]);
+$def[4] .= rrd::cdef("total", "active,idle,+");
+$def[4] .= rrd::line2("active", $main_color, "Active") ;
+$def[4] .= rrd::gprint("active", array("LAST", "AVERAGE", "MAX"), "%.0lf") ;
+$def[4] .= rrd::line2("total", $black, "Total") ;
+$def[4] .= rrd::gprint("total", array("LAST", "AVERAGE", "MAX"), "%.0lf") ;
  
 ?>
